a48d4b4ee9301c9606425f608aa608c5030e7ae3
zharick-aragon <157712903+zharick-aragon@users.noreply.github.com>
2024-09-10 09:16:55 -0500

    Add files via upload

---
+++ piezas.java
@@ -0,0 +1,25 @@
+
+public abstract class piezas {
+	//Atributos 
+	private static String nombre;
+	private static String color;
+	private static String movimiento;
+	public int posicionFila; 
+	public int posicionColumna; 
+	
+	//metodos 
+	public abstract String get1nombre(); 
+	public int getposicionFila; 
+	public String get1movimiento;
+	
+	public piezas(String nombre, int posicionFila, int posicionColumna) {
+	
+		this.nombre = nombre ;
+		this.posicionFila = posicionFila; 
+		this.posicionColumna = posicionColumna; 
+		} 
+	
+
+	}
+
+
